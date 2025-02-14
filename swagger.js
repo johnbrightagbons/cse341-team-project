5c13704bfbfd2c8ec09f26d996d765690c1ae8fd
VinoliaMoekwa <moe23001@byui.edu>
2025-02-14 15:53:17 +0200

    swagger files

--- swagger.js
+++ swagger.js
@@ -1,30 +1,28 @@
-const route = require('express').Router();
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('../swagger-output.json');
-
 const swaggerAutogen = require('swagger-autogen')();
 
 const doc = {
     info: {
-        title: "School API",
-        description: "API for managing students",
-
+        title: 'Academic Details Api',
+        description: 'Academical Details Api'   
     },
-    host: 'localhost:8080',
-    schemes: ['https', 'http'],
+    info: {
+        title: 'Class Info Api',
+        description: 'Class Info Api'   
+    },
+    info: {
+        title: 'Finances Api',
+        description: 'Finances Api'   
+    },
+    info: {
+        title: 'Students Api',
+        description: 'Students Api'   
+    },
+
+    host: 'localhost:3000',
+    schemes: ['https, http']
 };
 
+const outputFile = './swagger.json';
+const endpointsFiles = ['./routes/index.js']
 
-// const outputFile = require('../swagger-output.json');
-const endpointsFiles = ['./routes/index.js']; // Ensure this file contains your route definitions
-route.use('/api-docs', swaggerUi.serve);
-route.get('/api-docs', swaggerUi.setup(swaggerDocument));
-
-
-
-// Generate Swagger documentation
-swaggerAutogen(outputFile, endpointsFiles).then(() => {
-    console.log('Swagger documentation generated');
-});
-
-module.exports = route;
+swaggerAutogen(outputFile, endpointsFiles,doc);
