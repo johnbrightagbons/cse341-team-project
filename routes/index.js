b742551b2273f873159be2568c0b2fcfdbc47c36
VinoliaMoekwa <moe23001@byui.edu>
2025-02-15 21:40:51 +0200

    module router

--- routes/index.js
+++ routes/index.js
@@ -10,3 +10,5 @@ router.use('/academicDetails', require('./academicDetails'));
 router.use('/classInfo', require('./classInfo'));
 router.use('/finances', require('./finances'));
 router.use('/students', require('./students'));
+
+module.exports = router; 
