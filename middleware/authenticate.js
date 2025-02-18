000de05c30f40ab098aae0af431792ee94da42e2
VinoliaMoekwa <moe23001@byui.edu>
2025-02-18 22:35:25 +0200

    .

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,13 +1,21 @@
 const isAuthenticated = (req, res, next) => {
-    if (!req.isAuthenticated()) {
-        return res.status(401).json({ message: "You do not have access." });
+    try {
+        if (!req.isAuthenticated()) {
+            return res.status(401).json({ message: "You do not have access." });
+        }
+        next(); // Call next() only if the user is authenticated
+    } catch (error) {
+        res.status(500).json({ message: "Internal Server Error" });
     }
-    next();
 };
+
+module.exports = isAuthenticated;
+
+
 
 //const isAuthenticated = (req, res, next) => {
     //if (req.session.user === undefined) {
-        return res.status(401).json("You do not have access.");
+        //return res.status(401).json("You do not have access.");
     //}
     //next();
 //};
