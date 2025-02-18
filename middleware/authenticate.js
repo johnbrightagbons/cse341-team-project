0e16c08611ad48cdb3eb33df082f9b9c6f23677a
VinoliaMoekwa <moe23001@byui.edu>
2025-02-18 22:27:51 +0200

    .

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,9 +1,16 @@
 const isAuthenticated = (req, res, next) => {
-
-    if (req.session.user === undefined) {
-        return res.status(401).json("You do not have access.");
+    if (!req.isAuthenticated()) {
+        return res.status(401).json({ message: "You do not have access." });
     }
     next();
 };
+
+//const isAuthenticated = (req, res, next) => {
+    //if (req.session.user === undefined) {
+        return res.status(401).json("You do not have access.");
+    //}
+    //next();
+//};
+
 
 module.exports = { isAuthenticated };
--- routes/index.js
+++ routes/index.js
@@ -2,18 +2,29 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 
+app.use((req, res, next) => {
+  console.log("Session data:", req.session);
+  console.log("Authenticated user:", req.user);
+  next();
+});
+
+
 
 router.use('/academicDetails', require('./academicDetails'));
 router.use('/classInfo', require('./classInfo'));
 router.use('/finances', require('./finances'));
 router.use('/students', require('./students'));
 
+
+
 // Login route: triggers GitHub authentication.
-router.get('/login', (req, res, next) => {
-    console.log('Login route hit');
-    passport.authenticate('github')(req, res, next);
-  });
-  
+//router.get('/login', (req, res, next) => {
+    //console.log('Login route hit');
+    //passport.authenticate('github')(req, res, next);
+  //});
+  router.get('/login', passport.authenticate('github', { scope: ['user:email'] }));
+
+
   // GitHub callback route
   router.get('/github/callback', 
     passport.authenticate('github', { failureRedirect: '/' }),
@@ -25,13 +36,23 @@ router.get('/login', (req, res, next) => {
   );
   
   // Logout route: logs the user out and clears the session.
+  //router.get('/logout', (req, res, next) => {
+    //req.logout(function(err) {
+      //if (err) {
+        //return next(err);
+      //}
+      //res.redirect('/');
+    //});
+  //});
+
   router.get('/logout', (req, res, next) => {
-    req.logout(function(err) {
-      if (err) {
-        return next(err);
-      }
-      res.redirect('/');
+    req.logout();
+    req.session.destroy((err) => {
+        if (err) {
+            return next(err);
+        }
+        res.redirect('/');
     });
-  });
+});
 
 module.exports = router; 
