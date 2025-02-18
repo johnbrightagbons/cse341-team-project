d8b3d9f8f44165155ee8ceb6dae4969ea7ca0866
VinoliaMoekwa <moe23001@byui.edu>
2025-02-18 16:37:16 +0200

    . routes

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,30 @@ router.use('/classInfo', require('./classInfo'));
 router.use('/finances', require('./finances'));
 router.use('/students', require('./students'));
 
+// Login route: triggers GitHub authentication.
+router.get('/login', (req, res, next) => {
+    console.log('Login route hit');
+    passport.authenticate('github')(req, res, next);
+  });
+  
+  // GitHub callback route
+  router.get('/github/callback', 
+    passport.authenticate('github', { failureRedirect: '/' }),
+    (_req, res) => {
+      console.log('GitHub authentication successful');
+      // Successful authentication, redirect home.
+      res.redirect('/');
+    }
+  );
+  
+  // Logout route: logs the user out and clears the session.
+  router.get('/logout', (req, res, next) => {
+    req.logout(function(err) {
+      if (err) {
+        return next(err);
+      }
+      res.redirect('/');
+    });
+  });
+
 module.exports = router; 
